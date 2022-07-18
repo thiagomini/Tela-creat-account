df26ef12ab76a42d6ff588870351f41cc898748c
Thiago Valentim <51245643+thiagomini@users.noreply.github.com>
2022-07-18 15:14:15 -0300

    Update index.js
    Aqui seguem algumas dicas minhas para alterar seu script `js`. Deixei alguns exemplos de como você pode melhorar a legibilidade do seu código.  Tente fazer as outras partes que deixei como exercício :)

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ function checkInputs() {
     const passwordValue = password.value.trim()
     const passwordtwoValue = passwordtwo.value.trim()
 
-    if(usernameValue === '') {
+    // Substitui a expressão que você usava no if para ser essa função utilitária `isEmpty`. Dessa forma, você escreve menos e é até mais legível :)
+    // Além disso, fica como exercício você tentar extrait essas verificações do username para uma função chamada `validateUsername`. Utilize os exemplos
+    // abaixo como guias. No final, a intenção é que essa função `checkInputs` fique bem pequena e bem legível. O que importa ao leitor, à priori, é
+    // saber quais campos você quer validar. Deixe os detalhes de como cada campo é validado em suas próprias funções!
+    
+    if (isEmpty(usernameValue)) {
         // mostrar erro
         // add classe
         setErrorFor(username, 'Preencha esse campo')
@@ -26,41 +31,56 @@ function checkInputs() {
         setSuccessFor(username)
     }
 
-    if(emailValue === '') {
-        // mostrar erro
-        // add classe
-        setErrorFor(email, 'Preencha esse campo')
-    } else if (!isEmail(emailValue)) {
-        setErrorFor(email, 'Email inválido')
-    } else {
-        // adicionar a classe de sucesso
-        setSuccessFor(email)
-    }
-   
-    if(passwordValue === '') {
-        // mostrar erro
-        // add classe
-        setErrorFor(password, 'Preencha esse campo')
-
-    } else if(passwordValue.length < 8) { 
-        setErrorFor(password, 'Senha deve ter mais que 8 caracteres')
-    } else {
-        // adicionar a classe de sucesso
-        setSuccessFor(password)
-    }
-
+    // Tirei a validação aqui e coloquei em uma função que está definida abaixo. Dessa forma, o código fica menor aqui e mais expressivo, pois
+    // sabemos que queremos validar o email. Quando colocamos muitos detalhes de código em uma só função, pode ficar confuso de entender.
+    this.validateEmail(emailValue)
+    this.validatePassword(passwordValue) 
+  
+    
+    // Extrair essa verificação para uma função chamada `validatePasswordConfirmation`
     if(passwordtwoValue === '') {
         // mostrar erro
         // add classe
         setErrorFor(passwordtwo, 'Preencha esse campo')
 
-    } else if(passwordValue !== passwordtwoValue) { 
+    } else if (passwordValue !== passwordtwoValue) { 
         setErrorFor(passwordtwo, 'Senhas não tão iguais')
     } else {
         // adicionar a classe de sucesso
         setSuccessFor(passwordtwo)
     }
 
+}
+
+function validateEmail(emailValue) {
+  if (isEmpty(emailValue)) {
+    // mostrar erro
+    // add classe
+    setErrorFor(email, 'Preencha esse campo')
+  } else if (!isEmail(emailValue)) {
+      setErrorFor(email, 'Email inválido')
+  } else {
+      // adicionar a classe de sucesso
+      setSuccessFor(email)
+  }
+}
+
+function validatePassword(passwordValue) {
+  if (isEmpty(passwordValue)) {
+        // mostrar erro
+        // add classe
+        setErrorFor(password, 'Preencha esse campo')
+
+    } else if (passwordValue.length < 8) { 
+        setErrorFor(password, 'Senha deve ter mais que 8 caracteres')
+    } else {
+        // adicionar a classe de sucesso
+        setSuccessFor(password)
+    }
+}
+
+function isEmpty(textValue) { 
+    return textValue === ''
 }
 
 function setErrorFor(input, message) {
